6f23f08d087e2736e45a8a395eb08f8258366c42
Khalil1243 <khalil313che@gmail.com>
2025-03-27 19:15:31 -0400

    Update Ship.java

--- battleship/app/src/main/java/bcc/battleship/Ship.java
+++ battleship/app/src/main/java/bcc/battleship/Ship.java
@@ -1,65 +1,103 @@
 package bcc.battleship;
 
-public class Ship {
+public class Ship
+{
+    public static final int UNSET = -1;
+    public static final int HORIZONTAL = 0;
+    public static final int VERTICAL = 1;
+    private int row = UNSET;
+    private int col = UNSET;
+    private int length = UNSET;
+    private int direction = UNSET;
 
     // Constructor. Create a ship and set the length.
-    public Ship(int length) {
+    public Ship(int theLength)
+    {
+        length = theLength;
     }
 
     // Has the location been initialized
-    public boolean isLocationSet() {
-        return false;
+    public boolean isLocationSet()
+    {
+        if(row == UNSET || col == UNSET)
+        {
+            return false;
+        }
+        return true;
     }
-
     // Has the direction been initialized
-    public boolean isDirectionSet() {
-        return false;
+    public boolean isDirectionSet()
+    {
+        if(direction == UNSET)
+        {
+            return false;
+        }
+        return true;
     }
-
+    
     // Set the location of the ship
-    public void setLocation(int row, int col) {
-
+    public void setLocation(int r, int c)
+    {
+        row = r;
+        col = c;
     }
 
     // Set the direction of the ship
-    public void setDirection(int direction) {
-
+    public void setDirection(int d)
+    {
+        direction = d;
     }
-
+    
     // Getter for the row value
-    public int getRow() {
-        return 0;
+    public int getRow()
+    {
+        return row;
     }
 
     // Getter for the column value
-    public int getCol() {
-        return 0;
+    public int getCol()
+    {
+        return col;
     }
 
     // Getter for the length of the ship
-    public int getLength() {
-        return 0;
+    public int getLength()
+    {
+        return length;
     }
 
     // Getter for the direction
-    public int getDirection() {
-        return 0;
+    public int getDirection()
+    {
+        return direction;
     }
 
     // Helper method to get a string value from the direction
-    // Helper method to get a string value from the direction
-    private String directionToString() {
-        return "";
+    private String directionToString()
+    {
+        if(direction == HORIZONTAL)
+        {
+            return "horizontal";
+        }
+        if(direction == VERTICAL)
+        {
+            return "vertical; 
+        }
+        return "unset direction";
     }
 
     // Helper method to get a (row, col) string value from the location
-    private String locationToString() {
-        return "";
+    private String locationToString()
+    {
+        if(isDirectionSet())
+        {
+            return "(" + row + ", " + col + ")";
+        }
+        return "(unset location)";
     }
-
-    // toString value for this Ship
-    @Override
-    public String toString() {
-        return "";
+    // tostring value for this Ship
+    public String tostring()
+    {
+        return directionToString() + " ship of length " + length + " at " + locationToString();
     }
 }
