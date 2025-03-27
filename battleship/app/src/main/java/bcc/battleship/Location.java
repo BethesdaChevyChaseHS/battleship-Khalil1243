2b8a7daa3ff14eeb467bb5fd804805f0ebb5d7fe
Khalil1243 <khalil313che@gmail.com>
2025-03-27 19:16:41 -0400

    Update Location.java

--- battleship/app/src/main/java/bcc/battleship/Location.java
+++ battleship/app/src/main/java/bcc/battleship/Location.java
@@ -1,55 +1,91 @@
 package bcc.battleship;
 
-public class Location {
-    // Instance variables
+public class Location 
+{
+
+    // possible stata
+    public static final int UNGUESSED = 0;
+    public static final int HIT= 1;
+    public static final int MISSED = 2;
+
+    //Implement the Location class here
+    private boolean shipHere = false;
+    private int status = UNGUESSED;
 
     // Constructor. Initializes with no ship and status UNGUESSED.
-    public Location() {
+    public Location() 
+    {
 
     }
 
     // Was this Location a hit?
-    public boolean checkHit() {
+    public boolean checkHit() 
+    {
+        if(status == HIT)
+        {
+            return true;
+        }
         return false;
     }
 
     // Was this location a miss?
-    public boolean checkMiss() {
+    public boolean checkMiss() 
+    {
+        if(status == MISSED)
+        {
+            return true;
+        }
         return false;
     }
 
     // Was this location unguessed?
-    public boolean isUnguessed() {
+    public boolean isUnguessed() 
+    {
+        if(status == UNGUESSED)
+        {
+            return true;
+        }
         return false;
     }
 
     // Mark this location as a hit.
-    public void markHit() {
+    public void markHit() 
+    {
+        status = HIT;
 
     }
 
     // Mark this location as a miss.
-    public void markMiss() {
+    public void markMiss() 
+    {
+        status = MISSED;
 
     }
 
     // Return whether or not this location has a ship.
-    public boolean hasShip() {
-        return false;
+    public boolean hasShip() 
+    {
+        return shipHere;
     }
 
     // Set whether this location has a ship.
-    public void setShip(boolean val) {
+    public void setShip(boolean val) 
+    {
+        shipHere = val;
 
     }
 
     // Set the status of this Location.
-    public void setStatus(int status) {
+    public void setStatus(int theStatus) 
+    {
+        status = theStatus;
 
     }
 
     // Get the status of this Location.
-    public int getStatus() {
-        return 0;
+    public int getStatus() 
+    {
+        return status;
     }
 }
+
